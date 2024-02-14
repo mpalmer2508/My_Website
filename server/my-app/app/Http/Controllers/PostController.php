965704ffe7972618959ea0fa095923ed104eac57
mpalmer2508 <palmermatthew645@gmail.com>
2024-02-13 17:01:50 -0700

    Laravel and PostgreSQL

---
+++ server/my-app/app/Http/Controllers/PostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response; //import the Response class
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+}
