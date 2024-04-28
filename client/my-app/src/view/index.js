747de3c7fd4b04b3f9d829470d412cfc31bbbce4
mpalmer2508 <palmermatthew645@gmail.com>
2024-04-28 07:43:25 -0600

    get website live

--- client/my-app/src/view/index.js
+++ client/my-app/src/view/index.js
@@ -1,10 +1,15 @@
+import axios from "axios";
+
 function Home() {
 
     var main_page = (
         <div className="home_body">
             <h1>Hello World</h1>
+            <h2>axios.get('/')</h2>
         </div>
     );
+
+
 
     return main_page;
 }
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+?>
+<main>
+    <?php header('location:/client/my-app/public/index.html'); ?>
+</main>
