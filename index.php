321c258502d0ef803434e5caec754f2137c72471
mpalmer2508 <palmermatthew645@gmail.com>
2024-05-04 21:01:19 -0600

    Merge branch 'main' of https://github.com/mpalmer2508/My_Website

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 // include 'view/header.php';
@@ -7,23 +6,3 @@ session_start();
     <?php header('location:/predictions/index.php'); ?>
 </main>
 <!-- <?php include 'view/footer.php'; ?> -->
-=======
-<?php 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <a href="predictions/index.php">EPL Predictions</a>
-    <?php header("predictions/index.php")?>
-</body>
-
-</html>
->>>>>>> 69629f3bbda593da9eb8ee9930ad80112e250127
--- predictions/index.php
+++ predictions/index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
 <body>
-    <<<<<<< HEAD <a href="predictions/index">EPL Predictions</a>
+    <a>Predictions</a>
 </body>
-
-</html>
->>>>>>>> 69629f3bbda593da9eb8ee9930ad80112e250127:index.html
-=======
-<p>Predictions</p>
-</body>
-
-</html>
->>>>>>> 69629f3bbda593da9eb8ee9930ad80112e250127
